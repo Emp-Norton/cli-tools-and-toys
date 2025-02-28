4b8f9c8080a7644ae443d5b4d05b672ab03a5b97
James Jelenko <jamesjelenko@gmail.com>
2025-02-28 01:58:15 -0800

    add basic try-catch block for bash scripting

--- bash_shortcuts/functions.sh
+++ bash_shortcuts/functions.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# Borrowed (but I plan to return it, with only slight wear and tear) from the extremely talented Michaël Ferrand.
+# Modified for my own purposes.
+function try(){
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw(){
+    exit $1
+}
+
+function catch(){
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+function throwErrors(){
+    set -e
+}
+
+function ignoreErrors(){
+    set +e
+}
+
 wake_ssh () {
   echo -e "Attempting to rouse the lazy little dingo.\nThe person responsible for keeping him awake will be sacked.";
   eval $(ssh-agent -s);
