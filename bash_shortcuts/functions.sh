ccc95feac4cd28f0b9e17014d3b6e0266ceee2c6
James Jelenko <jamesjelenko@gmail.com>
2025-02-28 02:45:37 -0800

    refactored try-catch approach for functionality and concision/clarity.

--- bash_shortcuts/functions.sh
+++ bash_shortcuts/functions.sh
@@ -7,43 +7,37 @@ function try(){
     set +e
 }
 
-function throw(){
-    exit $1
-}
-
-function catch(){
-    export ex_code=$?
-    (( $SAVED_OPT_E )) && set +e
-    return $ex_code
-}
-
-function throwErrors(){
-    set -e
-}
-
-function ignoreErrors(){
-    set +e
+function catch() {
+  ex_code=$?
+  case $ex_code in
+    0)
+      echo -e "By jove, he's got it! He's really got it!"
+      ;;
+    1)
+      echo -e "Failed to rouse the dingo. The dingorouser will be sacked forthwith."
+    ;;
+    2)
+      echo -e "Failed to add the identity file. Shadda had it but diddna. The shaddahaddabuhdiddna will be sacked forthwith."
+    ;;
+    *)
+      echo -e "I'm stumped, ol' chap. Beware of this and that. Self-sacking imminent. Bip-bop and such."
+      return $ex_code
+    ;;
+  esac
 }
 
 function wake_ssh(){
-  try(
-    echo -e "Attempting to rouse the lazy little dingo.\nThe person responsible for keeping him awake will be sacked.";
-    [ eval $(ssh-agent -s); ] && throw $failedToRouseException
-    echo -e "Ooh he's feisty when he's sleepy, but he's up now! Let's see about having him add that identity file, shall we?";
-    ssh-add ~/.ssh/id_ed25519 || throw $failedToAddIdentityFile
-    echo -e "By jove, he's got it! He's really got it!";
-  ) catch || {
-      case $ex_code in
-        $failedToRouseException)
-          echo -e "Failed to rouse the dingo. The dingorouser will be sacked forthwith."
-        ;;
-        $failedToAddIdentityFile)
-          echo -e "Failed to add the identity file. Shadda had it but diddna. The shaddahaddabuhdiddna will be sacked forthwith."
-        ;;
-        *)
-          echo -e "I'm stumped, ol' chap. Beware of this and that. Self-sacking imminent. Bip-bop and such."
-          throw $ex_code
-        ;;
-      esac
-  }
+  local ex_code=0;
+
+  trap 'catch' err
+
+  echo -e "Attempting to rouse the lazy little dingo.\nThe person responsible for keeping him awake will be sacked.";
+  [[ -z "$(ssh-agent -s)" ]] && return 1
+
+  echo -e "Ooh he's feisty when he's sleepy, but he's up now! Let's see about having him add that identity file, shall we?";
+  ssh-add ~/.ssh/id_ed25519 || return 2
+
+  trap - ERR
+  catch
 }
+
