afce462797d0e1225c7a3e813f6e7a1d98ac0235
James Jelenko <jamesjelenko@gmail.com>
2025-02-28 02:23:04 -0800

    implement try-catch and add error messages

--- bash_shortcuts/functions.sh
+++ bash_shortcuts/functions.sh
@@ -25,10 +25,25 @@ function ignoreErrors(){
     set +e
 }
 
-wake_ssh () {
-  echo -e "Attempting to rouse the lazy little dingo.\nThe person responsible for keeping him awake will be sacked.";
-  eval $(ssh-agent -s);
-  echo -e "Ooh he's feisty when he's sleepy, but he's up now! Let's see about having him add that identity file, shall we?";
-  ssh-add ~/.ssh/id_ed25519;
-  echo -e "By jove, he's got it! He's really got it!";
+function wake_ssh(){
+  try(
+    echo -e "Attempting to rouse the lazy little dingo.\nThe person responsible for keeping him awake will be sacked.";
+    [ eval $(ssh-agent -s); ] && throw $failedToRouseException
+    echo -e "Ooh he's feisty when he's sleepy, but he's up now! Let's see about having him add that identity file, shall we?";
+    ssh-add ~/.ssh/id_ed25519 || throw $failedToAddIdentityFile
+    echo -e "By jove, he's got it! He's really got it!";
+  ) catch || {
+      case $ex_code in
+        $failedToRouseException)
+          echo -e "Failed to rouse the dingo. The dingorouser will be sacked forthwith."
+        ;;
+        $failedToAddIdentityFile)
+          echo -e "Failed to add the identity file. Shadda had it but diddna. The shaddahaddabuhdiddna will be sacked forthwith."
+        ;;
+        *)
+          echo -e "I'm stumped, ol' chap. Beware of this and that. Self-sacking imminent. Bip-bop and such."
+          throw $ex_code
+        ;;
+      esac
+  }
 }
