3f6987c1595e73779ff64eefb501c8ad6fa0df79
James Jelenko <jamesjelenko@gmail.com>
2025-03-18 00:45:52 -0700

    remove symlink to windows file, replace with text file

--- in_prog/js_console_helpers.js
+++ in_prog/js_console_helpers.js
@@ -1,1 +1,35 @@
-/mnt/d/js_timesavers.js
+const getNextButton = (selector='.btn-large.nextButton') => {
+	try(){
+		document.querySelector(selector);
+	} catch(err){
+		console.log(err);
+	} finally {
+		console.log("Here, kid, have a button.");
+	}
+}
+
+const clickNext = (nextSelector) => {
+    const nxtBtn = document.querySelector(nextSelector)
+}
+
+
+
+const watchElementPosition = (element) => {
+    const element = document.getElementById('myElement');
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                const currentLeft = element.style.left;
+
+                console.log('Left position changed to:', currentLeft);
+
+      }
+    }
+  });
+});
+
+    const config = { attributes: true, attributeFilter: ['style'] };
+
+    observer.observe(element, config);
+}
